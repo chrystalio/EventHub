9f3db259cfab82cf71392577557faa2af72010c1
chrystalio <chrystalio.k@outlook.com>
2025-06-15 19:45:35 +0700

    Clean up unused roles and add new roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,26 +13,8 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::firstOrCreate(['name' => 'System Administrator']);
-        Role::firstOrCreate(['name' => 'user']);
-        Role::firstOrCreate(['name' => 'guest']);
-        Role::firstOrCreate(['name' => 'super-admin']);
-        Role::firstOrCreate(['name' => 'admin']);
-        Role::firstOrCreate(['name' => 'editor']);
-        Role::firstOrCreate(['name' => 'viewer']);
-        Role::firstOrCreate(['name' => 'contributor']);
-        Role::firstOrCreate(['name' => 'moderator']);
-        Role::firstOrCreate(['name' => 'support']);
-        Role::firstOrCreate(['name' => 'manager']);
-        Role::firstOrCreate(['name' => 'developer']);
-        Role::firstOrCreate(['name' => 'analyst']);
-        Role::firstOrCreate(['name' => 'designer']);
-        Role::firstOrCreate(['name' => 'tester']);
-        Role::firstOrCreate(['name' => 'sales']);
-        Role::firstOrCreate(['name' => 'marketing']);
-        Role::firstOrCreate(['name' => 'hr']);
-        Role::firstOrCreate(['name' => 'finance']);
-        Role::firstOrCreate(['name' => 'legal']);
-        Role::firstOrCreate(['name' => 'operations']);
-        Role::firstOrCreate(['name' => 'support-agent']);
+        Role::firstOrCreate(['name' => 'Akademik']);
+        Role::firstOrCreate(['name' => 'Panitia']);
+        Role::firstOrCreate(['name' => 'Peserta']);
     }
 }
