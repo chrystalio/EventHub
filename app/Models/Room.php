5982df014ecca7855a5d5a80ee66a4d1c3b70101
chrystalio <chrystalio.k@outlook.com>
2025-06-17 08:46:53 +0700

    Add Room model

---
+++ app/Models/Room.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Room extends Model
+{
+    protected $fillable = [
+        'code',
+        'name',
+        'building_id',
+    ];
+
+    public function building(): BelongsTo
+    {
+        return $this->belongsTo(Building::class);
+    }
+}
