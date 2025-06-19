b952ff76459cb7b31925ff45e74d66b213ff3966
chrystalio <chrystalio.k@outlook.com>
2025-06-19 23:55:38 +0700

    Add relation to event

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
@@ -16,5 +17,10 @@ class Room extends Model
     public function building(): BelongsTo
     {
         return $this->belongsTo(Building::class);
+    }
+
+    public function events(): HasMany
+    {
+        return $this->hasMany(Event::class);
     }
 }
