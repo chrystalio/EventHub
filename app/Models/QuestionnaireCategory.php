88cb5a45d6c9b19a3001cbee71c62d7d76c46051
chrystalio <chrystalio.k@outlook.com>
2025-07-12 11:44:20 +0700

    Add `QuestionnaireCategory` model

---
+++ app/Models/QuestionnaireCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class QuestionnaireCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_active',
+    ];
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class)->orderBy('order_column');
+    }
+}
