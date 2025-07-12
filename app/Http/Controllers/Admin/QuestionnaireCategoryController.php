74a848025a6bcf5ebea229fef12fa02af2779322
chrystalio <chrystalio.k@outlook.com>
2025-07-12 11:54:18 +0700

    Declare return type

--- app/Http/Controllers/Admin/QuestionnaireCategoryController.php
+++ app/Http/Controllers/Admin/QuestionnaireCategoryController.php
@@ -9,10 +9,11 @@ use App\Models\QuestionnaireCategory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class QuestionnaireCategoryController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
         $questionnaireCategories = QuestionnaireCategory::latest()->get();
 
