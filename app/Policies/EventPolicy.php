f43ff7fec5d4153f6c56a1bfe51c928e6d2745e7
chrystalio <chrystalio.k@outlook.com>
2025-07-24 03:00:38 +0700

    Refactor code and remove ray debug code

--- app/Policies/EventPolicy.php
+++ app/Policies/EventPolicy.php
@@ -10,27 +10,12 @@ class EventPolicy
 {
     public function manage(User $user, Event $event): bool
     {
-        ray([
-            'message' => 'Checking authorization in EventPolicy@manage',
-            'user_being_checked' => [
-                'uuid' => $user->uuid,
-                'name' => $user->name,
-                'has_panitia_role' => $user->hasRole('Panitia'),
-            ],
-            'event_being_checked' => [
-                'uuid' => $event->uuid,
-                'name' => $event->name,
-            ],
-            'is_user_assigned_to_this_event' => $event->staff()->where('user_uuid', $user->uuid)->exists(),
-            'all_staff_ids_for_this_event' => $event->staff()->pluck('users.uuid')->all(),
-        ]);
-
         if ($user->hasRole(['System Administrator', 'Akademik'])) {
             return true;
         }
 
         if ($user->hasRole('Panitia')) {
-            return $event->staff()->where('user_uuid', $user->uuid)->exists();
+            return $event->staff->contains($user);
         }
 
         return false;
