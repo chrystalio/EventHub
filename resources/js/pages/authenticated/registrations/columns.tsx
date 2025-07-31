3c3e9d9c74592f77ff1ef57998bf9a4219c0eb57
chrystalio <chrystalio.k@outlook.com>
2025-07-31 17:49:51 +0700

    Implement `statusBadge` component

--- resources/js/pages/authenticated/registrations/columns.tsx
+++ resources/js/pages/authenticated/registrations/columns.tsx
@@ -7,27 +7,18 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Link } from "@inertiajs/react"
 import { ArrowUpDown } from "lucide-react"
+import { StatusBadge } from '@/components/ui/status-badge';
 
 const SortableHeader = <TData, TValue>({ column, title }: { column: Column<TData, TValue>, title: string }) => {
     return (
         <Button
             variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
             {title}
             <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
     )
 }
-
-const statusConfig: Record<Registration['status'], { bg: string, text: string }> = {
-    approved: { bg: 'bg-green-100', text: 'text-green-800' },
-    pending: { bg: 'bg-yellow-100', text: 'text-yellow-800' },
-    pending_payment: { bg: 'bg-yellow-200', text: 'text-yellow-900' },
-    rejected: { bg: 'bg-red-100', text: 'text-red-800' },
-    attended: { bg: 'bg-blue-100', text: 'text-blue-800' },
-    missed: { bg: 'bg-gray-100', text: 'text-gray-800' },
-};
 
 export const columns: ColumnDef<Registration>[] = [
     {
@@ -59,12 +50,7 @@ export const columns: ColumnDef<Registration>[] = [
         accessorKey: "status",
         header: "Status",
         cell: ({ row }) => {
-            const status = row.original.status;
-            return (
-                <Badge className={`capitalize ${statusConfig[status]?.bg || ''} ${statusConfig[status]?.text || ''}`}>
-                    {status}
-                </Badge>
-            )
+            return <StatusBadge status={row.original.status} />
         },
     },
     {
@@ -74,7 +60,6 @@ export const columns: ColumnDef<Registration>[] = [
             if (registration.status === 'pending_payment') {
                 return (
                     <div className="text-right">
-                        {/* Using the default primary button style */}
                         <Button
                             size="sm"
                             asChild
