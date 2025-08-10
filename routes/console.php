4120081a3dec02b644285568d032c50ac58d2411
chrystalio <chrystalio.k@outlook.com>
2025-08-10 19:43:42 +0700

    Refactor console

--- routes/console.php
+++ routes/console.php
@@ -3,13 +3,27 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote');
 
 
-Schedule::command('registrations:mark-missed')->hourly()->withoutOverlapping()->onFailure(function () {
-    Log::error('Failed to mark missed registrations.');
-})->onSuccess(function () {
-    Log::info('Successfully marked missed registrations.');
-});
+Schedule::command('registrations:mark-missed')
+    ->hourly()
+    ->withoutOverlapping()
+    ->onFailure(function () {
+        Log::error('[registrations:mark-missed] Failed to mark missed registrations at ' . now()->toDateTimeString());
+    })
+    ->onSuccess(function () {
+        Log::info('[registrations:mark-missed] Successfully completed marking missed registrations at ' . now()->toDateTimeString());
+    });
+
+Schedule::command('registrations:reject-private-pending')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->onFailure(function () {
+        Log::error('[registrations:reject-private-pending] Failed to reject pending private registrations at ' . now()->toDateTimeString());
+    })
+    ->onSuccess(function () {
+        Log::info('[registrations:reject-private-pending] Successfully completed rejection of pending private registrations at ' . now()->toDateTimeString());
+    });
