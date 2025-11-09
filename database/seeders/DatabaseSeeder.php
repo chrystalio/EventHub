f227bdf42a1e147ccf4cece69ce0448c5233d465
Chrystalio <chrystalio.k@outlook.com>
2025-11-09 16:05:36 +0700

    Add RolePermissionSeeder to seeder execution order for documentation and backwards compatibility

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             AdminUserSeeder::class,
             PermissionSeeder::class,
+            RolePermissionSeeder::class,
 
             BuildingSeeder::class,
             RoomSeeder::class,
